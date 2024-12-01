a853ce9b7c16539e8308977e3c9796bc6e35cde2
cristiancm49 <ccortesm2000@gmail.com>
2024-12-01 12:45:49 -0500

    first commit

---
+++ EJER1/POO/POO/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Apuntador apuntador = new Apuntador();
+
+class Apuntador
+{
+    string Color;
+    double Largo;
+    short NumeroBotones;
+    bool TieneStickers;
+
+};
