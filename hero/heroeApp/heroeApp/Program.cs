bf47b45cc0d4a2b6004eb9f32d73d65d96a45928
cristiancm49 <ccortesm2000@gmail.com>
2024-12-01 19:30:39 -0500

    pruebas linq

--- hero/heroeApp/heroeApp/Program.cs
+++ hero/heroeApp/heroeApp/Program.cs
@@ -1,2 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+var superman = new superHero();
+
+superman.ciudad = "florencia";
+superman.id = 1;
+superman.name = "SuperMan";
+superman.identidadSecreta = "Clark";
+superman.puedeVolar = true;
+superman.poderes = new[] { "fuerza", "velocidad", "rayos" };
+
+
+
+class superHero
+{
+    public int id;
+    public String name;
+    public string identidadSecreta;
+    public string ciudad;
+    public String[] poderes;
+    public bool puedeVolar;
+}
+
+Console.WriteLine(superman);
+
---
+++ pruebaLinq/linqQueries.cs
@@ -0,0 +1,5 @@
+public class LinqQuieries {
+    public LinqQuieries() {
+
+    }
+}
