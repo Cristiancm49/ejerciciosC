990c1d31d93b95c8a7b02c9cb476bcd91ef0e977
cristiancm49 <ccortesm2000@gmail.com>
2024-12-01 19:42:43 -0500

    pruebas linq

--- pruebaLinq/Program.cs
+++ pruebaLinq/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+LinqQueries queries = new LinqQueries();
+
+void ImprimirValores(IEnumerable<Book> GetLibros){
+    Console.WriteLine();
+}
--- pruebaLinq/linqQueries.cs
+++ pruebaLinq/linqQueries.cs
@@ -1,5 +1,13 @@
 public class LinqQuieries {
+    private List<Book> librosCollection = new List<Book>();
     public LinqQuieries() {
+        using(StreamReader reader = new StreamReader("books.json")){
+            string json = reader.ReadToEnd();
+            this.librosCollection = System.Text.Json.JsonConvert.Deserialize<List<Book>>(json, new System.Text.Json.JsonSerializerOptions() {PropertyNameCaseInsensitive = true});
+        }
+    }
 
+    public IEnumerable<Book> GetLibros() {
+        return librosCollection;
     }
 }
